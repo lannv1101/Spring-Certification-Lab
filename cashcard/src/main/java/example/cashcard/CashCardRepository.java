2c11f06d047946c989a2a0fc2902d66fb2747c3d
Lan Vu Nguyen <87882009+lannv1101@users.noreply.github.com>
2025-03-22 14:00:29 +0700

    init Beginer SpringBoot RestAPI

---
+++ cashcard/src/main/java/example/cashcard/CashCardRepository.java
@@ -0,0 +1,14 @@
+package example.cashcard;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.security.Principal;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
+    boolean existsByIdAndOwner(Long id, Principal principal);
+}
